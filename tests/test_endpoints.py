89489c7bff46ff1f9004699e09a7f23cb94c1e72
Kora <korastuff@gmail.com>
2021-10-04 12:11:27 -0400

    Unittest Practice
    ~learning to generated failed server tests by editing makefile and adding test_endpoints to /tests

---
+++ tests/test_endpoints.py
@@ -0,0 +1,12 @@
+from unittest import TestCase, skip
+
+import API.endpoints as endpoints
+
+
+class EndpointTestCase(TestCase):
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
+    def test_hellp(self):
+        self.assertTrue(False)
